2b9ff33cdc1f9e9369f7a432fcea741c2a480682
dmitry <dmitry.mm@icloud.com>
2021-08-30 23:23:27 +0300

    update junit to 4

--- src/test/java/guru/springfamework/api/v1/mapper/CategoryMapperTest.java
+++ src/test/java/guru/springfamework/api/v1/mapper/CategoryMapperTest.java
@@ -2,15 +2,18 @@ package guru.springfamework.api.v1.mapper;
 
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.domain.Category;
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class CategoryMapperTest extends TestCase{
+import static org.junit.Assert.assertEquals;
+
+public class CategoryMapperTest {
 
     public static final String NAME = "Some name";
     public static final long ID = 1L;
     CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
 
-    public void testCategoryToCategoryDTO() {
+    @Test
+    public void categoryToCategoryDTO() {
         Category category = new Category();
         category.setName(NAME);
         category.setId(ID);
